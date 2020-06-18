36d8965baaa7c3262d3d9eb5f030e022580fe107
michhottinger <michhottinger@users.noreply.github.com>
2020-06-18 15:13:51 -0700

    homework

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,13 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
+    lst = set()
+    for i in arr:
+        if i in lst:
+            lst.remove(i)
+        else:
+            lst.add(i)
+    return list(lst)[0]
 
 
 if __name__ == '__main__':
@@ -16,3 +20,19 @@ if __name__ == '__main__':
     
 
 
+# def single_number(arr):
+#     s = set() 
+#     # use either a dictionary or a set 
+#     # sets: holding onto unique elements 
+#     # loop through our arr
+#     for x in arr:
+#         # for each element
+#         # check if it is already in our set
+#         # if it is, then that's not our out-element-out 
+#         if x in s:
+#             # remove the element from our set 
+#             s.remove(x)
+#         else:
+#             s.add(x)
+#     # the odd-element-out will be the only element in the set 
+#     return list(s)[0]
