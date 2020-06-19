709df5add3b9e961d6e3c58862085dcdf675d176
michhottinger <michhottinger@users.noreply.github.com>
2020-06-19 11:05:26 -0700

    added notes from lecture on cache for future use

--- eating_cookies/.ipynb_checkpoints/eating_cookies-checkpoint.py
+++ eating_cookies/.ipynb_checkpoints/eating_cookies-checkpoint.py
@@ -16,6 +16,38 @@ def eating_cookies(n, ate=0):
             result += eating_cookies(n, ate+cookies) 
     return result
 
+
+#Runtime: O(3^n)
+# def eating_cookies(n):
+#     if n < 0:
+#         return 0
+#     elif n == 0:
+#         return 1
+#     else:
+#         return eating_cookies(n-3) + eating_cookies(n-2)+ eating_cookies(n-1)
+
+# the cache allows us to save our work 
+# cache is a dictionary where keys is the n, value is the answer 
+# Runtime: O(n)
+def eating_cookies(n, cache):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    # check if the answer is in our cache 
+    elif cache[n] > 0:
+        return cache[n]
+    else:
+        # otherwise, our cache doesn't contain the answer, so we'll use our 
+        # recursive logic to calculate it, and then save the answer in our 
+        # cache for future uses 
+        cache[n] = eating_cookies(n-3, cache) + eating_cookies(n-2, cache)+ eating_cookies(n-1, cache) 
+    return cache[n]
+
+
+
+
+
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 10
